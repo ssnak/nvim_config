b6eeb6267f9a9128c86f1f0793087c555a9c8755
JSLearner37525 <sergiog61826@portchesterschools.org>
2024-04-17 17:35:04 -0400

    added custom/init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 require 'core.mappings'
 require 'core.settings'
 require 'core.misc'
+require 'custom'
 
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
