682ce67ee27408f79c589d7fd1fa2856b56c7cc1
JSLearner37525 <sergiog61826@portchesterschools.org>
2024-04-18 18:35:26 -0400

    add harpoon2

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -63,6 +63,41 @@ return {
     end,
   },
   {
+
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local harpoon = require 'harpoon'
+      harpoon:setup()
+
+      local function nmap(lhs, rhs, opts)
+        opts = opts or {}
+        vim.keymap.set('n', lhs, rhs, opts)
+      end
+      nmap('<leader>aa', function()
+        harpoon:list():add()
+      end, { desc = 'Harpoon [A]dd' })
+      nmap('<leader>aj', function()
+        harpoon:list():select(1)
+      end, { desc = 'Harpoon Select 1' })
+      nmap('<leader>ak', function()
+        harpoon:list():select(2)
+      end, { desc = 'Harpoon Select 1' })
+      nmap('<leader>al', function()
+        harpoon:list():select(3)
+      end, { desc = 'Harpoon Select 1' })
+      nmap('<leader>a;', function()
+        harpoon:list():select(4)
+      end, { desc = 'Harpoon Select 1' })
+      nmap('<leader>ah', function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end, { desc = 'Harpoon menu' })
+    end,
+  },
+
+  -- local plugins
+  {
     'snak/runner.nvim',
     dependencies = {
       'MunifTanjim/nui.nvim',
