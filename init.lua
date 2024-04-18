7a07be687a0772691aeb890c6b71df875f13f108
JSLearner37525 <sergiog61826@portchesterschools.org>
2024-04-17 21:18:18 -0400

    setup project folder for local plugins

--- init.lua
+++ init.lua
@@ -11,6 +11,10 @@ end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup(require 'core.plugins', {
+  dev = {
+    path = '~/projects',
+    patterns = { 'snak' },
+  },
   ui = {
     icons = vim.g.have_nerd_font and {} or {
       cmd = '⌘',
