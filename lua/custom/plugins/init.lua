b93b78ffa0fe0beb21c97ea502ef37e83bb23c25
ssnak <sergregarias@gmail.com>
2024-04-19 18:35:02 -0400

    style: indent change to 4, modify: runner.nvim config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,151 +1,167 @@
 return {
-  {
-    'stevearc/oil.nvim',
-    opts = {},
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require('oil').setup()
-      vim.keymap.set('n', '<leader>o', '<cmd>Oil<cr>', { desc = 'Open [O]il' })
-    end,
-  },
-  {
-    'windwp/nvim-autopairs',
-    event = 'InsertEnter',
-    config = true,
-  },
-  {
-    'johmsalas/text-case.nvim',
-    dependencies = { 'nvim-telescope/telescope.nvim' },
-    config = function()
-      require('textcase').setup {}
-      require('telescope').load_extension 'textcase'
-    end,
-    keys = {
-      'ga', -- Default invocation prefix
-      { 'ga.', '<cmd>TextCaseOpenTelescope<CR>', mode = { 'n', 'x' }, desc = 'Telescope' },
+    {
+        "stevearc/oil.nvim",
+        opts = {},
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("oil").setup()
+            vim.keymap.set(
+                "n",
+                "<leader>o",
+                "<cmd>Oil<cr>",
+                { desc = "Open [O]il" }
+            )
+        end,
     },
-  },
-  {
-    'kdheepak/lazygit.nvim',
-    cmd = {
-      'LazyGit',
-      'LazyGitConfig',
-      'LazyGitCurrentFile',
-      'LazyGitFilter',
-      'LazyGitFilterCurrentFile',
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = true,
     },
-    -- optional for floating window border decoration
-    dependencies = {
-      'nvim-lua/plenary.nvim',
+    {
+        "johmsalas/text-case.nvim",
+        dependencies = { "nvim-telescope/telescope.nvim" },
+        config = function()
+            require("textcase").setup {}
+            require("telescope").load_extension "textcase"
+        end,
+        keys = {
+            "ga", -- Default invocation prefix
+            {
+                "ga.",
+                "<cmd>TextCaseOpenTelescope<CR>",
+                mode = { "n", "x" },
+                desc = "Telescope",
+            },
+        },
     },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
-    keys = {
-      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    {
+        "kdheepak/lazygit.nvim",
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        -- setting the keybinding for LazyGit with 'keys' is recommended in
+        -- order to load the plugin when the command is run for the first time
+        keys = {
+            {
+                "<leader>lg",
+                "<cmd>LazyGit<cr>",
+                desc = "LazyGit",
+            },
+        },
     },
-  },
-  {
-    'folke/noice.nvim',
-    event = 'VeryLazy',
-    opts = {
-      ---@type NoicePresets
-      presets = {},
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+            ---@type NoicePresets
+            presets = {},
+        },
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
+            "nvim-telescope/telescope.nvim",
+        },
+        config = function(opts)
+            require("noice").setup(opts)
+            require("telescope").load_extension "noice"
+            vim.keymap.set(
+                "n",
+                "<leader>sm",
+                "<cmd>Telescope noice<cr>",
+                { desc = "[S]earch [M]essages" }
+            )
+        end,
     },
-    dependencies = {
-      'MunifTanjim/nui.nvim',
-      'rcarriga/nvim-notify',
-      'nvim-telescope/telescope.nvim',
-    },
-    config = function(opts)
-      require('noice').setup(opts)
-      require('telescope').load_extension 'noice'
-      vim.keymap.set('n', '<leader>sm', '<cmd>Telescope noice<cr>', { desc = '[S]earch [M]essages' })
-    end,
-  },
-  {
+    {
 
-    'ThePrimeagen/harpoon',
-    branch = 'harpoon2',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      local harpoon = require 'harpoon'
-      harpoon:setup()
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local harpoon = require "harpoon"
+            harpoon:setup()
 
-      local function nmap(lhs, rhs, opts)
-        opts = opts or {}
-        vim.keymap.set('n', lhs, rhs, opts)
-      end
-      nmap('<leader>aa', function()
-        harpoon:list():add()
-      end, { desc = 'Harpoon [A]dd' })
-      nmap('<leader>aj', function()
-        harpoon:list():select(1)
-      end, { desc = 'Harpoon Select 1' })
-      nmap('<leader>ak', function()
-        harpoon:list():select(2)
-      end, { desc = 'Harpoon Select 1' })
-      nmap('<leader>al', function()
-        harpoon:list():select(3)
-      end, { desc = 'Harpoon Select 1' })
-      nmap('<leader>a;', function()
-        harpoon:list():select(4)
-      end, { desc = 'Harpoon Select 1' })
-      nmap('<leader>ah', function()
-        harpoon.ui:toggle_quick_menu(harpoon:list())
-      end, { desc = 'Harpoon menu' })
-    end,
-  },
-  {
-    'folke/trouble.nvim',
-    branch = 'dev', -- IMPORTANT!
-    keys = {
-      {
-        '<leader>xx',
-        '<cmd>Trouble diagnostics toggle<cr>',
-        desc = 'Diagnostics (Trouble)',
-      },
-      {
-        '<leader>xX',
-        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
-        desc = 'Buffer Diagnostics (Trouble)',
-      },
-      {
-        '<leader>cs',
-        '<cmd>Trouble symbols toggle focus=false<cr>',
-        desc = 'Symbols (Trouble)',
-      },
-      {
-        '<leader>cl',
-        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
-        desc = 'LSP Definitions / references / ... (Trouble)',
-      },
-      {
-        '<leader>xL',
-        '<cmd>Trouble loclist toggle<cr>',
-        desc = 'Location List (Trouble)',
-      },
-      {
-        '<leader>xQ',
-        '<cmd>Trouble qflist toggle<cr>',
-        desc = 'Quickfix List (Trouble)',
-      },
+            local function nmap(lhs, rhs, opts)
+                opts = opts or {}
+                vim.keymap.set("n", lhs, rhs, opts)
+            end
+            nmap("<leader>aa", function()
+                harpoon:list():add()
+            end, { desc = "Harpoon [A]dd" })
+            nmap("<leader>aj", function()
+                harpoon:list():select(1)
+            end, { desc = "Harpoon Select 1" })
+            nmap("<leader>ak", function()
+                harpoon:list():select(2)
+            end, { desc = "Harpoon Select 1" })
+            nmap("<leader>al", function()
+                harpoon:list():select(3)
+            end, { desc = "Harpoon Select 1" })
+            nmap("<leader>a;", function()
+                harpoon:list():select(4)
+            end, { desc = "Harpoon Select 1" })
+            nmap("<leader>ah", function()
+                harpoon.ui:toggle_quick_menu(harpoon:list())
+            end, { desc = "Harpoon menu" })
+        end,
     },
-    opts = {}, -- for default options, refer to the configuration section for custom setup.
-  },
-  {
-    'max397574/better-escape.nvim',
-    config = true,
-  },
+    {
+        "folke/trouble.nvim",
+        branch = "dev", -- IMPORTANT!
+        keys = {
+            {
+                "<leader>xx",
+                "<cmd>Trouble diagnostics toggle<cr>",
+                desc = "Diagnostics (Trouble)",
+            },
+            {
+                "<leader>xX",
+                "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+                desc = "Buffer Diagnostics (Trouble)",
+            },
+            {
+                "<leader>cs",
+                "<cmd>Trouble symbols toggle focus=false<cr>",
+                desc = "Symbols (Trouble)",
+            },
+            {
+                "<leader>cl",
+                "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+                desc = "LSP Definitions / references / ... (Trouble)",
+            },
+            {
+                "<leader>xL",
+                "<cmd>Trouble loclist toggle<cr>",
+                desc = "Location List (Trouble)",
+            },
+            {
+                "<leader>xQ",
+                "<cmd>Trouble qflist toggle<cr>",
+                desc = "Quickfix List (Trouble)",
+            },
+        },
+        opts = {}, -- for default options, refer to the configuration section for custom setup.
+    },
+    {
+        "max397574/better-escape.nvim",
+        config = true,
+    },
 
-  -- local plugins
-  {
-    'snak/runner.nvim',
-    dependencies = {
-      'MunifTanjim/nui.nvim',
+    -- local plugins
+    {
+        "snak/runner.nvim",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+        },
+        opts = {},
     },
-    config = function()
-      require 'runner'
-    end,
-    enabled = true,
-  },
 }
