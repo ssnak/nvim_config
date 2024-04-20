2541ca37c357ee44979feca528caf9f9bbb3c469
ssnak <sergregarias@gmail.com>
2024-04-20 16:10:08 -0400

    add: overseer status to lualine

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -70,7 +70,12 @@ return {
     {
         "nvim-lualine/lualine.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
-        opts = { options = { theme = "tokyonight" } },
+        opts = {
+            options = { theme = "tokyonight" },
+            sections = {
+                lualine_x = { "encoding", "overseer", "filetype" },
+            },
+        },
     },
     {
         "stevearc/overseer.nvim",
