52b2fa4b058fd77d3aa9aefeaaf8e5a901fa597d
ssnak <sergregarias@gmail.com>
2024-04-20 10:00:55 -0400

    disable nvim.statusline

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -319,6 +319,7 @@ return {
     },
     {
         "echasnovski/mini.statusline",
+        enabled = false,
         config = function()
             local statusline = require "mini.statusline"
             statusline.setup { use_icons = vim.g.have_nerd_font }
