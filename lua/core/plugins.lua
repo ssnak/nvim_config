bcbe270cfe7b0d40a7ef0df0fcafd31b1440b23b
ssnak <sergregarias@gmail.com>
2024-04-20 14:24:09 -0400

    remove: mini.statusline

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -318,18 +318,6 @@ return {
         opts = { signs = false },
     },
     {
-        "echasnovski/mini.statusline",
-        enabled = false,
-        config = function()
-            local statusline = require "mini.statusline"
-            statusline.setup { use_icons = vim.g.have_nerd_font }
-            ---@diagnostic disable-next-line: duplicate-set-field
-            statusline.section_location = function()
-                return "%2l:%-2v"
-            end
-        end,
-    },
-    {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         opts = {
