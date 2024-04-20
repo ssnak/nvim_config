2a4db0e0afebbbdb7411f70fa0c590ee5d02009c
ssnak <sergregarias@gmail.com>
2024-04-20 00:01:22 -0400

    minor

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -1,8 +1,9 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 vim.g.have_nerd_font = true
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.wrap = false
 -- vim.opt.mouse = 'a'
 vim.opt.showmode = false
 -- vim.opt.clipboard = 'unnamedplus'
@@ -10,14 +11,14 @@ vim.opt.breakindent = true
 vim.opt.undofile = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-vim.opt.inccommand = 'split'
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.inccommand = "split"
 vim.opt.cursorline = true
 vim.opt.scrolloff = 10
 vim.opt.hlsearch = true
