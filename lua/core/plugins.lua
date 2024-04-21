4f2e600d93de6e24a8f7b4af1853de68cdae08f1
ssnak <sergregarias@gmail.com>
2024-04-21 12:26:38 -0400

    replace: luau-lsp.nvim for default lspconfig

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -111,12 +111,6 @@ return {
             { "folke/neoconf.nvim", opts = {} },
             { "j-hui/fidget.nvim", opts = {} },
             { "folke/neodev.nvim", opts = {} },
-            {
-                "lopi-py/luau-lsp.nvim",
-                dependencies = {
-                    "nvim-lua/plenary.nvim",
-                },
-            },
         },
         config = function()
             vim.api.nvim_create_autocmd("LspAttach", {
@@ -188,13 +182,6 @@ return {
                         server.capabilities =
                             vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
                         require("lspconfig")[server_name].setup(server)
-                    end,
-                    luau_lsp = function()
-                        require("luau-lsp").setup {
-                            fflags = {
-                                sync = false,
-                            },
-                        }
                     end,
                 },
             }
--- lua/core/servers.lua
+++ lua/core/servers.lua
@@ -1,25 +1,26 @@
 --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 local servers = {
-  clangd = {},
-  gopls = {},
-  pyright = {},
-  rust_analyzer = {},
-  tsserver = {},
-  powershell_es = {},
-  lua_ls = {
-    -- cmd = {...},
-    -- filetypes = { ...},
-    -- capabilities = {},
-    settings = {
-      Lua = {
-        completion = {
-          callSnippet = 'Replace',
+    clangd = {},
+    gopls = {},
+    pyright = {},
+    rust_analyzer = {},
+    tsserver = {},
+    powershell_es = {},
+    luau_lsp = {},
+    lua_ls = {
+        -- cmd = {...},
+        -- filetypes = { ...},
+        -- capabilities = {},
+        settings = {
+            Lua = {
+                completion = {
+                    callSnippet = "Replace",
+                },
+                -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+                -- diagnostics = { disable = { 'missing-fields' } },
+            },
         },
-        -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-        -- diagnostics = { disable = { 'missing-fields' } },
-      },
     },
-  },
 }
 
 return servers
