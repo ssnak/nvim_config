ab09372d16739e259694af06b489527b44361a6f
ssnak <sergregarias@gmail.com>
2024-10-28 20:53:16 -0400

    add keybind to switch between source and header file using clangd lsp

--- lua/custom/autocmds.lua
+++ lua/custom/autocmds.lua
@@ -1,1 +1,11 @@
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client.supports_method "textDocument/switchSourceHeader" then
+            vim.keymap.set("n", "gh", function()
+                vim.cmd.ClangdSwitchSourceHeader()
+            end, { desc = "Switch to header file" })
+        end
+    end,
+})
 return {}
