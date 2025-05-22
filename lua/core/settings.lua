5529e2ed4dfc6649aea111870eb2fb312a3c7063
ssnak <sergregarias@gmail.com>
2025-05-22 16:56:00 -0400

    fix: vim.o.listchars error, change to vim.opt.listchars

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -15,7 +15,7 @@ vim.o.timeoutlen = 300
 vim.o.splitright = true
 vim.o.splitbelow = true
 vim.o.list = true
-vim.o.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 vim.o.inccommand = "split"
 vim.o.cursorline = true
 vim.o.scrolloff = 10
