067910765ad81055c24c32481a4506f32940a52d
ssnak <sergregarias@gmail.com>
2025-05-24 13:07:08 -0400

    disable undefined-field warning on init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 require "core"
 
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+---@diagnostic disable-next-line: undefined-field
 if not vim.loop.fs_stat(lazypath) then
     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
     vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath }
